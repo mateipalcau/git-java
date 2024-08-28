1b3d971268fa5de85636e6cfe5b99de2f0beb5d6
Matei Palcau <mpalcau@stu.jsu.edu>
2024-08-28 09:27:45 -0500

    Initial Commit

---
+++ Class1.java
@@ -0,0 +1,18 @@
+public class Class1 {
+    private void f1() {
+    // Placeholder method (v1.0)
+    }
+    private void f2() {
+        // Placeholder method (v2.0)
+    }
+    private void f3() {
+        // Placeholder method (v2.0)
+        //made a change for hotfix
+    }
+    private void f4() {
+        // starting work on f4
+    }
+    private void f4() {
+        // Placeholder method (v2.0)
+    }
+}
